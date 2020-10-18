08ab01fd02946c248cdf4d5144a9bdac90a05648
RavinduDilhara <ravindu.1dilhara@gmail.com>
2020-10-18 20:21:21 +0530

    final1

--- __tests__/MainProps-test.js
+++ __tests__/MainProps-test.js
@@ -6,7 +6,6 @@ import renderer from 'react-test-renderer';
 import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
- 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('Testing Props', ()=> {
